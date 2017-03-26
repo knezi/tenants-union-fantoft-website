241e0a45a63d8d04c59e21114dd0a2d0c1890ff1
Asbjørn Thegler <asbjoern@gmail.com>
2017-03-26 16:37:02 +0200

    Adds first example of processing Markdown to HTML

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var express = require("express");
+var showdown  = require("showdown");
+var fs = require("fs");
 var log = console;
 var app = express();
 
+app.use((req, res,  next) => {
+    req.converter = new showdown.Converter();
+    next();
+});
+
 app.get("/", function(req, res) {
     res.send("Hello Fantoft!");
+});
+
+app.get("/markdown", function(req, res) {
+    var markdown = fs.readFileSync("./test.md").toString();
+    var html = req.converter.makeHtml(markdown);
+
+    res.send(html);
 });
 
 var port = 5075;
